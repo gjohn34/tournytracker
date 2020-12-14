50af6dca6b0e0995d9023c4a05b55de311e3ffd0
Glen Johnson <glenjohnson39@yahoo.com.au>
2020-12-14 13:48:12 +1000

    clean

--- TrackerUI/CreatePrizeForm.cs
+++ TrackerUI/CreatePrizeForm.cs
@@ -54,10 +54,7 @@ namespace TrackerUI
                     prizePercentageValue.Text
                 );
 
-                foreach (IDataConnection db in GlobalConfig.Connections)
-                {
-                    db.CreatePrize(model);
-                }
+                GlobalConfig.Connection.CreatePrize(model);
                 placeNameValue.Text = "";
                 placeNumberValue.Text = "";
                 prizeAmountValue.Text = "0";
