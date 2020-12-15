e4240e82a9f1bedb52a2f50175c677101768b959
Glen Johnson <glenjohnson39@yahoo.com.au>
2020-12-15 15:08:02 +1000

    text write

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -4,14 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Model;
+using TrackerLibrary.DataAccess.TextHelpers;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
+
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+            int lastId = 1;
+            if (prizes.Count > 0)
+            {
+                lastId = prizes.OrderByDescending(x => x.Id).First().Id;
+
+            }
+            model.Id = lastId + 1;
+
+            prizes.Add(model);
+
+            prizes.SaveToPrizesFile(PrizesFile);
+
             return model;
         }
 
---
+++ TrackerLibrary/DataAccess/TextConnectorProcessor.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary.Model;
+
+namespace TrackerLibrary.DataAccess.TextHelpers
+{
+    public static class TextConnectorProcessor
+    {
+        /// <summary>
+        /// Taking a file path and prepend the full filePath
+        /// </summary>
+        /// <param name="filename">eg PrizeModel.csv</param>
+        /// <returns></returns>
+        public static string FullFilePath(this string filename)
+        {
+            return $"{ConfigurationManager.AppSettings["filePath"]}\\{filename}";
+        }
+        public static List<string> LoadFile(this string file)
+        {
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            return File.ReadAllLines(file).ToList();
+        }
+
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+
+            foreach(string line in lines)
+            {
+                string[] cols = line.Split(',');
+
+                PrizeModel prize = new Model.PrizeModel();
+                prize.Id = int.Parse(cols[0]);
+                prize.PlaceNumber = int.Parse(cols[1]);
+                prize.PlaceName = cols[2];
+                prize.PrizeAmount = decimal.Parse(cols[3]);
+                prize.PrizePercentage = double.Parse(cols[4]);
+                output.Add(prize);
+            }
+            return output;
+        }
+
+        public static void SaveToPrizesFile(this List<PrizeModel> models, string fileName)
+        {
+            List<string> lines = new List<string>();
+            foreach (PrizeModel model in models)
+            {
+                lines.Add($"{model.Id},{model.PlaceNumber},{model.PlaceName},{model.PrizeAmount},{model.PrizePercentage}");
+            }
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
